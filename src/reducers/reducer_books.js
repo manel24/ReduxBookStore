08dab4abf8ef651723e7f440d363c56416955f45
manel24 <manelcharouni@think-it.io>
2018-07-02 17:57:13 +0100

    Redux+ React integration

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,19 @@
+export default function () {
+    return [{
+            title: 'Harry potter'
+        },
+        {
+            title: 'Harry potter2'
+        },
+        {
+            title: 'Harry potter3'
+        },
+        {
+            title: 'Harry potter4'
+        },
+        {
+            title: 'Harry potter5'
+        }
+    ]
+
+}
